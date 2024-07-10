e5c6911b51c04840f14552f4341e7acdc0892ca7
Rodrigo Silva <rodrigosilva.emcode@gmail.com>
2024-07-10 15:04:10 -0300

    Create Our ECommerce Models - Django Wednesdays ECommerce #2

--- project_ecommerce01/settings.py
+++ project_ecommerce01/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -105,6 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
