f9dcec19b37665855b15f9305e68c228f7540999
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2024-07-23 17:59:51 -0300

    Criando função de promoção na hora de mostrar o valor

--- store/models.py
+++ store/models.py
@@ -28,7 +28,12 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='upload/product/')
-
+    
+    #Add sale stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    
+    
     def __str__(self):
         return self.name
 
